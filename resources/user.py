577d4f545d4d8f628ece46b6e0a921d4813da7ea
Ishan Hemmige (Nokia) <ishan.hemmige@nokia.com>
2023-06-21 09:49:57 -0400

    first

---
+++ resources/user.py
@@ -0,0 +1,93 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required
+
+
+from db import db
+from models import UserModel
+from schemas import UserSchema, UserRegisterSchema
+from blocklist import BLOCKLIST
+import os
+import requests
+
+
+blp = Blueprint("Users", "users", description="Operations on users")
+
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    return requests.post(
+        f"https://api.mailgun.net/v3/{domain}/messages",
+        auth=("api", os.getenv("MAILGUN_API_KEY")),
+        data={
+            "from": f"Your Name <mailgun@{domain}>",
+            "to": [to],
+            "subject": subject,
+            "text": body,
+        },
+    )
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out"}, 200
+
+
+@blp.route("/register")
+class UserRegister(MethodView):
+    @blp.arguments(UserRegisterSchema)
+    def post(self, user_data):
+        if UserModel.query.filter(UserModel.username == user_data["username"],UserModel.email == user_data["email"]).first():
+            abort(409, message="A user with that username or email already exists.")
+
+        user = UserModel(
+            username=user_data["username"],
+            email=user_data["email"],
+            password=pbkdf2_sha256.hash(user_data["password"]),
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        send_simple_message(
+            to=user.email,
+            subject="Successfully signed up",
+            body=f"Hi {user.username}! You have successfully signed up to the Stores REST API."
+        )
+        return {"message": "User created successfully."}, 201
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
+
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=user.id)
+            return {"access_token": access_token}, 200
+
+        abort(401, message="Invalid credentials.")
+
+@blp.route("/user/<int:user_id>")
+class User(MethodView):
+    """
+    This resource can be useful when testing our Flask app.
+    We may not want to expose it to public users, but for the
+    sake of demonstration in this course, it can be useful
+    when we are manipulating data regarding the users.
+    """
+
+    @blp.response(200, UserSchema)
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return {"message": "User deleted."}, 200
